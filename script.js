6cba1ad0433725ed3f22352f81693753d0187a7a
aman <ay845790@gmail.com>
2026-02-02 22:17:35 +0530

    adding tha frunted

---
+++ script.js
@@ -0,0 +1,19 @@
+const slides = document.querySelector(".slides");
+const images = document.querySelectorAll(".slides img");
+
+let index = 0;
+const totalImages = images.length;
+
+function showSlide() {
+  slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+setInterval(() => {
+  index++;
+
+  if (index >= totalImages) {
+    index = 0;
+  }
+
+  showSlide();
+}, 2000);
